c04768816f497aab88cf2fc623c456b7ee646b24
Henrique Eidt <heidt@degdigital.com>
2020-01-30 15:37:51 -0300

    Add postcss-url and documentation to export config file

--- skeletor/export/css.config.js
+++ skeletor/export/css.config.js
@@ -1,5 +1,28 @@
+/**
+ *
+ * @const assetsMap {Object} mapping of local URLs and remote URLs or empty Object
+ * @note assetsMap needs to be an Object with the original url as the key and the new url as the value
+ * You can also create a JSON file elsewhere - such as assets.json - and require it instead of listing them here
+ * @example
+ * 	assetsMap = {
+ * 	    "images/my_image.png": "https://www.deg.com/image/on/the/server.png",
+ * 	    "images/my_second_image.png": "https://www.deg.com/second/image/on/the/server.png"
+ * 	    ...
+ * 	}
+ * 	*/
+
 const {files, plugins} = require('../common/css.config.js');
 const outputDir = 'export';
+
+const assetMap = {};
+
+function mapUrl(asset, dir) {
+	if(assetMap.hasOwnProperty(asset.url)) {
+		return assetMap[asset.url];
+	}
+
+	return asset.url;
+}
 
 module.exports = {
     name: 'css',
@@ -10,7 +33,12 @@ module.exports = {
                 files: [
                     ...files(`${outputDir}`)
                 ],
-                plugins
+                plugins: [
+					...plugins,
+					require('postcss-url')({
+						url: mapUrl
+					}),
+				]
             }
         }
     ]
